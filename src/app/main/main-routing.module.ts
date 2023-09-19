5082b9b5f0013dd32072576bc214c5c989af930f
phung.phanquoc <phung.phanquoc>
2023-09-19 21:30:21 +0700

    main works

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,29 +1,36 @@
-import { Routes } from "@angular/router";
-import { ProductComponent } from "./product/product.component";
-import { CategoryComponent } from "./category/category.component";
-import { UserComponent } from "./user/user.component";
-import { RoleComponent } from "./role/role.component";
-import { MainComponent } from "./main.component";
+import { Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { UserComponent } from './user/user.component';
+import { RoleComponent } from './role/role.component';
+import { MainComponent } from './main.component';
+import { HomeComponent } from './home/home.component';
 
 export const mainRoute: Routes = [
-    {
-        path: '', 
-        component: MainComponent
-    },
-    {
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
         path: 'product',
-        component: ProductComponent
-    },
-    {
+        component: ProductComponent,
+      },
+      {
         path: 'category',
-        component: CategoryComponent
-    },
-    {
-        path: "user",
-        component: UserComponent
-    },
-    {
+        component: CategoryComponent,
+      },
+      {
+        path: 'user',
+        component: UserComponent,
+      },
+      {
         path: 'role',
-        component: RoleComponent
-    }
-]
+        component: RoleComponent,
+      },
+    ],
+  },
+];
